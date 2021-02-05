97894e5bc96325a1e464ab656768a0fea76193ea
Tyler Stephans <tbs5111@psu.edu>
2021-02-05 11:19:13 -0500

    Added saving/loading a setup file

--- BIMpic_autoplanner.py
+++ BIMpic_autoplanner.py
@@ -1,11 +1,13 @@
 import open3d.open3d as o3d
 import numpy as np
+import pickle
 
 import voxel_labelling as vl
 
 np.random.seed(0)
 
 # Inputs
+fn_setup = "autoplanner_setup.pkl"
 fn_asBuilt = r"fused.ply"
 fn_asPlanned = r"corner_flat_s6.obj"
 fn_cameras = r"cameras.txt"
@@ -26,33 +28,52 @@ max_bound_coord = np.array([8.0, 8.5, 0.4])
 pcd_b = o3d.io.read_point_cloud(fn_asBuilt)
 print(pcd_b)
 
-print "Importing BIM"
-elements, mesh_p = vl.import_BIM(fn_asPlanned, min_bound_coord,
-                                 max_bound_coord, point_cloud_density)
+try:
+    # elements = []
+    # cameras = vl.camera()
+    # images = []
+    # pcd_p = o3d.geometry.PointCloud()
+    # voxelGrid_p = o3d.geometry.VoxelGrid()
+    # voxel_reference =
+    with open(fn_setup) as f_setup:
+        print "Loading autoplanner setup file"
+        (point_cloud_density, elements, cameras,
+         images, pcd_p, voxelGrid_p, voxel_reference) = pickle.load(f_setup)
+except IOError:
+    print "Importing BIM"
+    elements, mesh_p = vl.import_BIM(fn_asPlanned, min_bound_coord,
+                                     max_bound_coord, point_cloud_density)
 
-print "Importing COLMAP meta files"
-cameras = vl.import_cameras_file(fn_cameras)
+    print "Importing COLMAP meta files"
+    cameras = vl.import_cameras_file(fn_cameras)
 
-images = vl.import_images_file(fn_images)
+    images = vl.import_images_file(fn_images)
 
-# Create voxel grid from element point clouds
-pcd_p = o3d.geometry.PointCloud()
-for element in elements:
-    pcd_p = pcd_p + element['point_cloud']
+    # Create voxel grid from element point clouds
+    pcd_p = o3d.geometry.PointCloud()
+    for element in elements:
+        pcd_p = pcd_p + element['point_cloud']
 
-voxelGrid_p = o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(
-    pcd_p, voxel_size, min_bound_coord, max_bound_coord
-)
-print(voxelGrid_p)
+    voxelGrid_p = o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(
+        pcd_p, voxel_size, min_bound_coord, max_bound_coord
+    )
+    print(voxelGrid_p)
 
-# Convert voxel grid into custom format to save labels
-voxel_reference = vl.voxel_label_reference(voxelGrid_p,
-                                           min_bound_coord, max_bound_coord)
+    # Convert voxel grid into custom format to save labels
+    voxel_reference = vl.voxel_label_reference(voxelGrid_p,
+                                               min_bound_coord,
+                                               max_bound_coord)
 
-# o3d.visualization.draw_geometries([voxelGrid_p, pcd_b])
+    # o3d.visualization.draw_geometries([voxelGrid_p, pcd_b])
 
-print "Labelling elements in reference voxel grid."
-voxel_reference.create_element_labels(elements)
+    print "Labelling elements in reference voxel grid."
+    voxel_reference.create_element_labels(elements)
+
+    print "Saving autoplanner setup file"
+    dump = [point_cloud_density, elements, cameras,
+            images, pcd_p, voxelGrid_p, voxel_reference]
+    with open(fn_setup, 'w') as f_setup:
+        pickle.dump(dump, f_setup)
 
 print "Creating labelled voxel"
 voxel_labelled = voxel_reference.export_voxel_labelled(range(len(elements)))
