46ecb50384b08ad7e31f0a3caf6c43fcd73fd005
Tyler Stephans <tbs5111@psu.edu>
2021-02-05 10:38:37 -0500

    Corrected BIMpic_auto and renamed open3d_extra

--- BIMpic_autoplanner.py
+++ BIMpic_autoplanner.py
@@ -1,8 +1,7 @@
 import open3d.open3d as o3d
 import numpy as np
-import copy
 
-import open3d_extra as o3de
+import voxel_labelling as vl
 
 np.random.seed(0)
 
@@ -27,81 +26,14 @@ max_bound_coord = np.array([8.0, 8.5, 0.4])
 pcd_b = o3d.io.read_point_cloud(fn_asBuilt)
 print(pcd_b)
 
-mesh_p = o3d.io.read_triangle_mesh(fn_asPlanned)
-print(mesh_p)
-
-# generate numpy array of vertices from as-planned model
-vert_p = np.asarray(mesh_p.vertices)
-
-# parse .obj file to find indices of named objects (building elements)
-print "Parsing *.obj file"
-elements = []
-
-with open(fn_asPlanned) as f_obj:
-    lines_obj = f_obj.readlines()
-
-n_elements = len([line for line in lines_obj if line.startswith('o ')])
-
-last_tag = 'v'
-k = 0
-for line in lines_obj:
-    if line.startswith('o '):
-        elements.append(dict(name=line[2:-1]))
-
-        last_tag = 'o'
-
-    if line.startswith('v '):
-        vert_cur = np.asarray(line[2:-1].split(' ')).astype('float')
-        diff_cur = vert_cur - vert_p[k, :]
-        if (np.abs(diff_cur) > 1E-5).any():
-            print("Warning: vertex %G differed by %s" %
-                  (k, np.array2string(diff_cur)))
-
-        if last_tag == 'o':
-            elements[-1]['v_index'] = [k]
-        else:
-            elements[-1]['v_index'].append(k)
-
-        last_tag = 'v'
-        k = k + 1
+print "Importing BIM"
+elements, mesh_p = vl.import_BIM(fn_asPlanned, min_bound_coord,
+                                 max_bound_coord, point_cloud_density)
 
 print "Importing COLMAP meta files"
-cameras = o3de.import_cameras_file(fn_cameras)
+cameras = vl.import_cameras_file(fn_cameras)
 
-images = o3de.import_images_file(fn_images)
-
-print "Generating point clouds and removing unwanted elements"
-# Loop through every named object from the obj in reverse order, remove onces
-# which fall outside the specified bounds, and save point clouds of the ones
-# that don't
-for k in range(len(elements)-1, -1, -1):
-    # Remove every other element from the triangle mesh by remove their
-    # vertices
-    to_remove = range(k) + range(k+1, len(elements))
-    vert_remove = [vert for index in to_remove for vert in
-                   elements[index]["v_index"]]
-    mesh_temp = copy.deepcopy(mesh_p)
-    mesh_temp.remove_vertices_by_index(vert_remove)
-
-    vert_temp = np.asarray(mesh_temp.vertices)
-    # If the current element is within the bounds of interest
-    if ((vert_temp >= min_bound_coord).all()
-            and (vert_temp <= max_bound_coord).all()):
-        # Calculate the area of the desired element.
-        # The area is used to ensure every element has a mostly equally dense
-        # point cloud.
-        n_pts = int(o3de.triangle_mesh_area(mesh_temp)/point_cloud_density)
-
-        # Add color for visual differentiation
-        elements[k]['color'] = np.random.rand(3)
-
-        print("Saved %s as point cloud with %G points."
-              % (elements[k]["name"], n_pts))
-        pcd_temp = mesh_temp.sample_points_poisson_disk(n_pts)
-        elements[k]['point_cloud'] = \
-            pcd_temp.paint_uniform_color(elements[k]['color'])
-    else:
-        del elements[k]
+images = vl.import_images_file(fn_images)
 
 # Create voxel grid from element point clouds
 pcd_p = o3d.geometry.PointCloud()
@@ -114,8 +46,8 @@ voxelGrid_p = o3d.geometry.VoxelGrid.create_from_point_cloud_within_bounds(
 print(voxelGrid_p)
 
 # Convert voxel grid into custom format to save labels
-voxel_reference = o3de.voxel_label_reference(voxelGrid_p,
-                                             min_bound_coord, max_bound_coord)
+voxel_reference = vl.voxel_label_reference(voxelGrid_p,
+                                           min_bound_coord, max_bound_coord)
 
 # o3d.visualization.draw_geometries([voxelGrid_p, pcd_b])
 
@@ -147,6 +79,9 @@ voxel_labelled = voxel_reference.export_voxel_labelled(range(len(elements)))
 print "Labeling blocked voxels in as-planned model."
 voxel_labelled.create_planned_labels(cameras, images)
 
-voxel_labelled.visualize("planned")
+print "Labeling occupied voxels in as-built model."
+voxel_labelled.create_built_labels(pcd_b)
+
+voxel_labelled.visualize("built", plot_geometry=[pcd_b])
 
 print('done!')
